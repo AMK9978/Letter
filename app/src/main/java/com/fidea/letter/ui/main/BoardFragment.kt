130be33b1e324099354d5aaaca546983f6cbdc1d
AmirMohammad Karimi <amk9978@gmail.com>
2020-06-15 14:56:38 +0430

    BoardsFragment added

--- app/src/main/java/com/fidea/letter/ui/main/BoardFragment.kt
+++ app/src/main/java/com/fidea/letter/ui/main/BoardFragment.kt
@@ -1,6 +1,20 @@
 package com.fidea.letter.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.fidea.letter.R
 
 class BoardFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.boards_fragment, container, false)
+        return view
+    }
 }
