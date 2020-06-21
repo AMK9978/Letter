80e4c7f78e53bfbc66fee8aaf52bcfa5b71baf19
AmirMohammad Karimi <amk9978@gmail.com>
2020-06-21 10:04:18 +0430

    new:Libraries for test

---
+++ app/src/main/java/com/fidea/letter/repositories/SearchRepository.kt
@@ -0,0 +1,79 @@
+package com.fidea.letter.repositories
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.fidea.letter.RxBus
+import com.fidea.letter.api.APIClient
+import com.fidea.letter.api.APIInterface
+import com.fidea.letter.models.Item
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+import javax.inject.Singleton
+
+@Singleton
+class SearchRepository {
+
+    companion object {
+        private var repo: SearchRepository? = null
+
+        fun getRepo(): SearchRepository? {
+            if (repo == null) {
+                return SearchRepository()
+            }
+            return repo
+        }
+    }
+
+
+    fun getContents(token: String?, cacheDir: File) {
+        val apiInterface = APIClient.getRetrofit(token, cacheDir)!!.create(APIInterface::class.java)
+        apiInterface.search()!!.enqueue(object :
+            Callback<java.util.ArrayList<Item>?> {
+            override fun onFailure(call: Call<java.util.ArrayList<Item>?>, t: Throwable) {
+                Log.i("TAG", "Error in getting items " + t.message)
+            }
+
+            override fun onResponse(
+                call: Call<java.util.ArrayList<Item>?>,
+                response: Response<java.util.ArrayList<Item>?>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    RxBus.getNewItems().onNext(response.body()!!)
+                } else {
+                    Log.i("TAG", "Errtttor in getting items ${response.code()}")
+                }
+            }
+
+        })
+    }
+
+    fun getFavorites(token: String?, cacheDir: File): MutableLiveData<ArrayList<Item>> {
+        val data = MutableLiveData<ArrayList<Item>>()
+        val apiInterface = APIClient.getRetrofit(token, cacheDir)!!.create(APIInterface::class.java)
+        apiInterface.getFavorites()!!.enqueue(object :
+            Callback<java.util.ArrayList<Item>?> {
+            override fun onFailure(call: Call<java.util.ArrayList<Item>?>, t: Throwable) {
+                Log.i("TAG", "Error in getting favorites")
+            }
+
+            override fun onResponse(
+                call: Call<java.util.ArrayList<Item>?>,
+                response: Response<java.util.ArrayList<Item>?>
+            ) {
+                if (response.isSuccessful) {
+                    data.value = response.body()
+                    Log.i("TAG", "HERE:" + response.body())
+                } else {
+                    Log.i("TAG", "Error in getting favorites ${response.code()}")
+                }
+            }
+
+        })
+
+        Log.i("TAG", "What about here? " + data.value)
+        return data
+    }
+
+}
--- app/src/test/java/com/fidea/letter/ExampleUnitTest.kt
+++ app/src/test/java/com/fidea/letter/ExampleUnitTest.kt
@@ -1,7 +1,13 @@
 package com.fidea.letter
 
+import android.R
+import android.content.Context
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,5 +18,20 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    private val FAKE_STRING = "HELLO WORLD"
+
+    @Mock
+    var mockContext: Context? = null
+
+    @Test
+    fun readStringFromContext_LocalizedString() { // Given a mocked Context injected into the object under test...
+//        when(mockContext?.getString(R.string.ok)).thenReturn(FAKE_STRING)
+//        val myObjectUnderTest = ClassUnderTest(mockContext)
+//        // ...when the string is returned from the object under test...
+//        val result: String = myObjectUnderTest.getHelloWorldString()
+//        // ...then the result should be the expected one.
+//        assertThat(result, is(FAKE_STRING))
     }
 }
